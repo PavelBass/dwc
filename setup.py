42cb83cdd468ef3df31aafed97d0261eca5b0ca5
Pavel Bass <statgg@gmail.com>
2017-08-15 09:17:25 +0300

    Version 1.0

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='dwc',
+      version='1.0',
+      description='DATADVANCE Words Calculator',
+      author='Pavel Bass',
+      author_email='statgg@gmail.com',
+      scripts=['dwc'],
+     )
